561ac520226b8734be7f57244c9565def887d9a5
Samin <samin99099@gmail.com>
2024-01-23 00:10:15 +0600

    structure updaated

---
+++ tic-tac-toe-two-player-online-game/src/main/java/com/example/tictactoetwoplayeronlinegame/TicTacToeTwoPlayerOnlineGameApplication.java
@@ -0,0 +1,13 @@
+package com.example.tictactoetwoplayeronlinegame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicTacToeTwoPlayerOnlineGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicTacToeTwoPlayerOnlineGameApplication.class, args);
+	}
+
+}
---
+++ tic-tac-toe-two-player-online-game/src/test/java/com/example/tictactoetwoplayeronlinegame/TicTacToeTwoPlayerOnlineGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tictactoetwoplayeronlinegame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicTacToeTwoPlayerOnlineGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
